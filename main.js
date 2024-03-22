397e59537ff7cde6f82c26c8daa92eb0816dc629
Cleber Prestes <85308692+CleberPrestes@users.noreply.github.com>
2024-03-21 22:06:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const botoes  = document.querySelectorAll(".botao");
 
+const textos  = document.querySelectorAll(".aba-conteudo");
+
+
 for (let i = 0; i < botoes.length; i++) {
 
 
@@ -7,10 +10,21 @@ for (let i = 0; i < botoes.length; i++) {
 
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
             
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+
     }
         
 }
+
+const contadores  = document.querySelectorAll(".contador");
+const tempoAtual = new Date(); // retorna a data atual do computador
+const tempoObjetivo1 =  new Date("2024-10-05T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+//document.querySelector("#minhaDiv p").textContent = "Que chuva forte";
